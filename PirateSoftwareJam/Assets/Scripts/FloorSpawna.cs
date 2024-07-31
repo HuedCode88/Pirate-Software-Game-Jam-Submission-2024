66b5517fe83979f69bc749b0769f0b19d61e8216
Alex Hued <66076614+HuedCode88@users.noreply.github.com>
2024-07-30 22:46:45 -0400

    Background happened

--- PirateSoftwareJam/Assets/Scripts/FloorSpawna.cs
+++ PirateSoftwareJam/Assets/Scripts/FloorSpawna.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class FloorSpawna : MonoBehaviour
 {
-    public static int minDist = 2000;
-    public static int maxDist = 4000;
+    public static int minDist = 200;
+    public static int maxDist = 400;
     public int width;
     public int height=1;
     [SerializeField] GameObject floor;
